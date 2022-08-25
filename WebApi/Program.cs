6f62c303a70e3bacf9e9543a9b3d3c1527c207d2
Francuz94 <Forward552@gmail.com>
2022-08-25 14:39:35 +0200

    Zapisanie Pracy

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,13 @@
+using Domain.Interfaces;
+using Infrastracter.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//AddScoped przy ka¿dym zapytyaniu tworzone jest nowa istancja klasy repository
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
 
 var app = builder.Build();
 
